a8c380a80494bca1bf8e46580e5686841f72fc5b
Corvo <391063482@qq.com>
2021-06-23 16:37:18 +0800

    wip

--- util/FFT.cpp
+++ util/FFT.cpp
@@ -1,21 +1,39 @@
 /*
  * @Author: sajuuk
  * @Date: 2021-06-22 20:55:44
- * @LastEditTime: 2021-06-23 11:06:10
+ * @LastEditTime: 2021-06-23 16:35:46
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \FFTGIFgenerater\util\FFT.cpp
  */
 #include "FFT.h"
 /**
- * @description: 
+ * @description: initialize the twiddle factor list and store the number of samples
+ * @param {int} N number of samples
+ * @return {*}
+ */
+void init(int n)
+{
+    N = n;
+    twiddleFactor = (complexNum*)malloc(sizeof(complexNum) * n);
+    complexNum temp=complexNum(1,0);
+    complexNum f=complexNum(cos(2*PI/n),-sin(2*PI/n));
+    for (int i = 0; i < n; i++)
+    {
+        twiddleFactor[i]=temp;
+        temp=temp*f;
+    }
+}
+/**
+ * @description: get the twiddle factor w(n,k)
  * @param {int} n, the primitive Nth root of unity
  * @param {int} k, power
  * @return {complexNum} the twiddle factor
  */
-complexNum getTwiddlefactor(int n,int k)
+complexNum getTwiddlefactor(int n, int k)
 {
-    
+    int t=N/n;
+    return twiddleFactor[k*t];//normalize the twiddle factor and query the factor list
 }
 /**
  * @description: Fast fourier transform
@@ -23,32 +41,38 @@ complexNum getTwiddlefactor(int n,int k)
  * @param {float*} pointer of input array, 
  * @return {*}
  */
-void ffTrans(int n,complexNum* input,complexNum* output)
+void ffTrans(int n, complexNum* input, complexNum* output)
 {
-    if(n==1) 
+    if (n == 1)
     {
-        (*output)=(*input);
+        (*output) = (*input);
         return;
     }
-    complexNum* xEven=(complexNum*)malloc(sizeof(complexNum)*(n/2));
-    complexNum* xOdd=(complexNum*)malloc(sizeof(complexNum)*(n/2));
-    for (int i=0;i<n;i++)
+    complexNum* xEven = (complexNum*)malloc(sizeof(complexNum) * (n / 2));
+    complexNum* xOdd = (complexNum*)malloc(sizeof(complexNum) * (n / 2));
+    for (int i = 0; i < n; i++)
     {
-        if (i%2) //i is odd
+        if (i % 2) //i is odd
         {
-            xOdd[i/2]=input[i];
+            xOdd[i / 2] = input[i];
         }
         else
         {
-            xEven[i/2]=input[i];
+            xEven[i / 2] = input[i];
         }
     }
-    complexNum* a=(complexNum*)malloc(sizeof(complexNum)*(n/2));
-    complexNum* b=(complexNum*)malloc(sizeof(complexNum)*(n/2));
-    ffTrans(n/2,xEven,a);
-    ffTrans(n/2,xOdd,b);
-    for (int i=0;i<n/2;i++)
+    complexNum* a = (complexNum *)malloc(sizeof(complexNum) * (n / 2));
+    complexNum* b = (complexNum *)malloc(sizeof(complexNum) * (n / 2));
+    ffTrans(n / 2, xEven, a);
+    ffTrans(n / 2, xOdd, b);
+    free(xEven);
+    free(xOdd);
+    for (int i = 0; i < n / 2; i++)
     {
-
+        b[i]=getTwiddlefactor(n,i)*b[i];
+        output[i]=a[i]+b[i];
+        output[i+n/2]=a[i]-b[i];
     }
+    free(a);
+    free(b);
 }
--- util/FFT.h
+++ util/FFT.h
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-22 20:55:44
- * @LastEditTime: 2021-06-23 09:59:19
+ * @LastEditTime: 2021-06-23 16:34:28
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \FFTGIFgenerater\util\FFT.h
@@ -13,15 +13,30 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+#define PI 3.14159265358979323846
 struct complexNum
 {
     float r;//real part
     float j;//image part
     friend complexNum operator* (complexNum& a,complexNum& b)
     {
-        complexNum temp;
+        complexNum temp(0,0);
         temp.r=a.r*b.r-a.j*b.j;
         temp.j=a.r*b.j+a.j*b.r;
+        return temp;
+    }
+    friend complexNum operator+ (complexNum& a,complexNum& b)
+    {
+        complexNum temp(0,0);
+        temp.r=a.r+b.r;
+        temp.j=a.j+b.j;
+        return temp;
+    }
+    friend complexNum operator- (complexNum& a,complexNum& b)
+    {
+        complexNum temp(0,0);
+        temp.r=a.r-b.r;
+        temp.j=a.j-b.j;
         return temp;
     }
     void operator=(complexNum& a)
@@ -29,8 +44,16 @@ struct complexNum
         r=a.r;
         j=a.j;
     }
+    complexNum(float rr,float jj)
+    {
+        r=rr;j=jj;
+    }
 };
+int N;//the number of samples
+complexNum* twiddleFactor;
 extern "C"
 {
+    void init(int n);
+    complexNum getTwiddlefactor(int n,int k);
     void ffTrans(int n,complexNum* input,complexNum* output);
 }
