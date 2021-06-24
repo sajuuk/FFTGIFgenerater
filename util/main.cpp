c4532bea5cc336136cc82dacc3d0a9f1498d11f7
Corvo <391063482@qq.com>
2021-06-24 10:00:48 +0800

    add test

---
+++ util/main.cpp
@@ -0,0 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-23 23:18:14
+ * @LastEditTime: 2021-06-23 23:21:15
+ * @LastEditors: Please set LastEditors
+ * @Description: test FFT
+ * @FilePath: \FFTGIFgenerater\util\main.cpp
+ */
+#include"FFT.h"
+#include
