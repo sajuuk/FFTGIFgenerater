95c20ff3b43fa4fe4f9e860fa4794a425b9527b5
Corvo <391063482@qq.com>
2021-06-25 22:27:16 +0800

    test the use of dll in py

--- src/run.py
+++ src/run.py
@@ -6,8 +6,20 @@ Created on Tue Jun 22 20:51:47 2021
 """
 
 from ctypes import *
+class complexNum(Structure):
+    _fields_=[('r',c_float),('j',c_float)]
 def main():
     FFTdll=CDLL('F:\\DA\\FFTGIFgenerater\\build\\libFFT.dll')
     print(FFTdll)
+    FFTdll.init(c_int(8))
+    datastore=complexNum*8
+    inputdata=datastore()
+    outputdata=datastore()
+    for i in inputdata:
+        i.r=int(input())
+        i.j=int(input())
+    fft=FFTdll.ffTrans(c_int(8),pointer(inputdata),pointer(outputdata))
+    for i in outputdata:
+        print("{0}+{1}i".format(i.r,i.j))
 if __name__ == "__main__":
     main()
